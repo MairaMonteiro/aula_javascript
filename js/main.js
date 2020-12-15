4f28b7286e71ec1359b1fe350259b17b869dad93
Maíra Monteiro <mairamonte@gmail.com>
2020-12-15 09:57:03 -0300

    trabalhando com js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+var nome = "Maira";
+var idade = 29;
+var idade2 = 10;
+var frase = "Japão é o melhor time"
+alert(nome + " tem " + idade + " anos");
+console.log(nome);
+console.log(idade);
+alert(frase.toUpperCase());
+//alert(frase.replace("Japão", "Brasil"))
