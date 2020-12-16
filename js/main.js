463ee0f768093eddfbefb1e97d31e56ac2a3f834
Maíra Monteiro <mairamonte@gmail.com>
2020-12-16 11:21:02 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,47 @@
 
-var nome = "Maira";
-var idade = 29;
-var idade2 = 10;
-var frase = "Japão é o melhor time"
-alert(nome + " tem " + idade + " anos");
-console.log(nome);
-console.log(idade);
-alert(frase.toUpperCase());
-//alert(frase.replace("Japão", "Brasil"))
+// var nome = "Maira";
+// var idade = 29;
+// var idade2 = 10;
+// var frase = "Japão é o melhor time"
+// alert(nome + " tem " + idade + " anos");
+// console.log(nome);
+// console.log(idade);
+// alert(frase.toUpperCase());
+// alert(frase.replace("Japão", "Brasil"))
+
+// var lista = ["moça", "pera", "laranja"];
+// console.log(lista[1]);
+// lista.push("uva");
+// console.log(lista.reverse())
+// //lista.pop();
+// console.log(lista.join(" - "));
+
+// var fruta = [{ nome:'maçã', cor:'vermelha'}, { nome:'uva', cor:'roxa' }];
+// console.log(fruta)
+// alert(fruta[1].cor)
+
+// var idade = prompt('Qual a sua idade?')
+// var idadeMinima = 18;
+// if (idade >= idadeMinima){
+//     alert('maior de idade')
+// } else {
+//     alert('menor de idade')
+// }
+
+// var count = 0;
+// while (count < 5){
+//     console.log(count);
+//     alert(count);
+//     count++
+// }
+
+// var count;
+// for (count = 0; count<=5; count++){
+//     alert(count)
+// }
+
+var d = new Date();
+console.log(d.getMonth()+1);
+console.log(d.getDay());
+console.log(d.getHours());
+console.log(d.getUTCDate());
